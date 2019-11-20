67a738571753c175232b4f510fb714e656134ea7
Jonathan Palma <jonathanpalma89@gmail.com>
2019-11-20 00:29:24 +0000

    set up Top_up & raise_error when balance limit exceeds

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,24 @@
 class Oystercard
+
+  BALANCE_LIMIT = 90
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+  def top_up(amount)
+    total = @balance + amount
+    raise over_balance_exceed(total) if total > BALANCE_LIMIT
+    @balance += amount 
+  end
+
+  private
+
+  def over_balance_exceed(total)
+    error = "Over balance limit exceed: #{total} / #{BALANCE_LIMIT}."
+    BalanceError.new(error)
+  end
+end
+
+class BalanceError < StandardError
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,31 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  subject(:card) { described_class.new } # give a name for the subject (class) -> card
+  let(:balance_limit) { Oystercard::BALANCE_LIMIT } # give a name for a constant variable
+
+  it 'return 0£ when create it' do
+    expect(card.balance).to eq 0
+  end
+
+  describe '#top up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'should return the balance' do # 1st time on top it up
+      expect { card.top_up(1) }.to change { card.balance }.by 1 
+    end
+
+    it 'should top up twice' do
+      card.top_up(2)
+      card.top_up(3)
+      expect(card.balance).to eq 5
+    end
+
+    context 'Raise an error when' do
+      it 'exceed the maximum amount' do
+        expect { card.top_up balance_limit+1}.to raise_error BalanceError, "Over balance limit exceed: #{balance_limit + 1} / #{balance_limit}."
+      end
+    end
+  end
 end
